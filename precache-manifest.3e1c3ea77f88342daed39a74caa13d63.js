0edb97e813dfcc9cff513ac66e029e95f560ea79
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-15 06:45:49 +0000

    Deploy XieZhiXian-NA/laicomponent to github.com/XieZhiXian-NA/laicomponent.git:gh-pages

---
+++ precache-manifest.3e1c3ea77f88342daed39a74caa13d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68751d85c5921c79ada9f44fec145fe0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.60d84233835cc78b7057.bundle.js"
+  },
+  {
+    "url": "runtime~main.60d84233835cc78b7057.bundle.js"
+  },
+  {
+    "revision": "90e1ce3edefd069153f2",
+    "url": "static/css/main.17833cd5.chunk.css"
+  },
+  {
+    "revision": "b09e12c6ce60bb3260aa",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.60d84233835cc78b7057.bundle.js"
+  },
+  {
+    "url": "vendors~main.60d84233835cc78b7057.bundle.js.LICENSE.txt"
+  }
+]);
