2bd891d0f20e646ecf76ebf9654b74cc99e446be
xzxfzt <1916004436@qq.com>
2020-05-15 11:24:39 +0800

    update travis.yml

--- dist/components/Icon/icon.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
-export declare type ThemePros = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark';
-export declare type IconProps = FontAwesomeIconProps & {
-    theme?: ThemePros;
-    className?: string;
-};
-export declare const Icon: FC<IconProps>;
-export default Icon;
